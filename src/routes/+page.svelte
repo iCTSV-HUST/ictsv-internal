0f641214823a70e3b47f9ba2f343a9b4dd6bd9b3
hwangisgone <95560238+hwangisgone@users.noreply.github.com>
2024-10-19 07:06:42 +0800

    fix page being flagged as phishing due to redirecting to login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang='ts'>
+    import { goto } from "$app/navigation";
+</script>
+
+<div class="container mx-auto mt-4">
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+	<button class="mt-4 btn btn-secondary" onclick={() => goto('/login')}>Đăng nhập</button>	
+</div>
+
+
