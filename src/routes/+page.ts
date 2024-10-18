402955f6b99d6bc2b413f808a9ce82700a2db5d4
hwangisgone <95560238+hwangisgone@users.noreply.github.com>
2024-10-18 15:23:50 +0800

    Update +page.ts

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,3 +1,6 @@
 import { redirect } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
 
-redirect(302, '/app');
+export const load: PageLoad = async () => {
+	redirect(302, '/app');
+}
